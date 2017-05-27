886d29c8081a42fb058b007db5944d40500ec16a
hcsun <markandlw@gmail.com>
2017-05-27 13:15:28 +0800

    Wrong initializing of vx,vy from radar measurement.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -87,9 +87,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       Convert radar from polar to cartesian coordinates and initialize state.
       */
       ekf_.x_ << measurement_pack.raw_measurements_[0] * cos(measurement_pack.raw_measurements_[1]),
-        measurement_pack.raw_measurements_[0] * sin(measurement_pack.raw_measurements_[1]),
-        measurement_pack.raw_measurements_[2] * cos(measurement_pack.raw_measurements_[1]),
-        measurement_pack.raw_measurements_[2] * sin(measurement_pack.raw_measurements_[1]);
+        measurement_pack.raw_measurements_[0] * sin(measurement_pack.raw_measurements_[1]), 0, 0;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
