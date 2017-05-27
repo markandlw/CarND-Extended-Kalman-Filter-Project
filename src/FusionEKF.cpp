88501e6ecf3cb952fd9da5504f6aa3b21b43a5b4
hcsun <markandlw@gmail.com>
2017-05-27 13:22:19 +0800

    Skip prediction step if delta time is too small.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -117,22 +117,24 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt - expressed in seconds
   previous_timestamp_ = measurement_pack.timestamp_;
 
-  float dt_2 = dt * dt;
-  float dt_3 = dt_2 * dt;
-  float dt_4 = dt_3 * dt;
+  if (dt > 0.000001) {
+    float dt_2 = dt * dt;
+    float dt_3 = dt_2 * dt;
+    float dt_4 = dt_3 * dt;
 
-  //Modify the F matrix so that the time is integrated
-  ekf_.F_(0, 2) = dt;
-  ekf_.F_(1, 3) = dt;
+    //Modify the F matrix so that the time is integrated
+    ekf_.F_(0, 2) = dt;
+    ekf_.F_(1, 3) = dt;
 
-  //set the process covariance matrix Q
-  float noise_ax = 9.0;
-  float noise_ay = 9.0;
-  ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
-         0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
-         dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
-         0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
-  ekf_.Predict();
+    //set the process covariance matrix Q
+    float noise_ax = 9.0;
+    float noise_ay = 9.0;
+    ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+           0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+           dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+           0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+    ekf_.Predict();
+  }
   /*****************************************************************************
    *  Update
    ****************************************************************************/
